6274f67d7ae79cde2cd3f20fcc3787d411f3dc31
enri stol <enrique.d.stol@gmail.com>
2025-05-06 18:53:02 -0300

    [challengue-tenpo]-initial scaffolding

---
+++ src/test/java/com/example/challengue_tenpo/ChallengueTenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengue_tenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengueTenpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
